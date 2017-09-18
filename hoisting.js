a6ee8b76dba36b63e270b66635ee394f59aefe35
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-18 19:44:35 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -20,14 +20,14 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  
+
   var name = "Cricky";
   var name = "Kristin";
 
 
   function sayMy() {
     console.log(name);
-    
+
   }
   sayMy();
 }
